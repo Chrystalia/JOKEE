3df84bac9e9c753ff69115c3364bd8c1ce712948
Chrystalia <glenysang@gmail.com>
2023-03-03 13:56:19 +0700

    validasi signUp di php bukan js. Kalau di js, meski kena alert tapi tetap masuk database

--- signUp.js
+++ signUp.js
@@ -8,38 +8,38 @@
         });
     
     
-    function validasi() {
-        var username = document.getElementById("set-username").value;  
-        var email = document.getElementById("set-email").value;  
-        var password = document.getElementById("set-password").value;  
-        var check = document.forms["formset"]["agree"].checked;
-        // var check = document.get
-        console.log("masuk sini")
-        if(username == "" && email == "" && password == "" && check == false){
-            alert("You must fill in the data completely!");
-            return false;
-            exit;
-        }else if(username == "" && email != "" && password != ""){
-            alert("You must fill in your username completely!");
-            return false;
-            exit;
-        }else if(username != "" && email == "" && password != ""){
-            alert("You must fill in your email completely!");
-            return false;
-            exit;
-        }else if(username != "" && email != "" && password == ""){
-            alert("You must fill in your password completely!");
-            return false;
-            exit;
-        }else if(!check){ 
-            alert("You must agree to the terms and conditions!");
-            return false;
-            exit;
-        }else if(username == "" && email == "" && password == "" && check == true){
-            alert("You must fill in the data completely!");
-            return false;
-            exit;
-        }
+    // function validasi() {
+    //     var username = document.getElementById("set-username").value;  
+    //     var email = document.getElementById("set-email").value;  
+    //     var password = document.getElementById("set-password").value;  
+    //     var check = document.forms["formset"]["agree"].checked;
+    //     // var check = document.get
+    //     console.log("masuk sini")
+    //     if(username == "" && email == "" && password == "" && check == false){
+    //         alert("You must fill in the data completely!");
+    //         return false;
+    //         exit;
+    //     }else if(username == "" && email != "" && password != ""){
+    //         alert("You must fill in your username completely!");
+    //         return false;
+    //         exit;
+    //     }else if(username != "" && email == "" && password != ""){
+    //         alert("You must fill in your email completely!");
+    //         return false;
+    //         exit;
+    //     }else if(username != "" && email != "" && password == ""){
+    //         alert("You must fill in your password completely!");
+    //         return false;
+    //         exit;
+    //     }else if(!check){ 
+    //         alert("You must agree to the terms and conditions!");
+    //         return false;
+    //         exit;
+    //     }else if(username == "" && email == "" && password == "" && check == true){
+    //         alert("You must fill in the data completely!");
+    //         return false;
+    //         exit;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
--- signUp.php
+++ signUp.php
@@ -22,49 +22,48 @@
 
     <h2 class="font-signin">Sign Up</h2>
 
-    <form action="" name="formset" method="post" class="formSubmit" onsubmit="return validasi()">
-        <center><div class="kotak-username"> 
-            <img src="images/Icon Us.svg" alt="" class="icon-person">
-            <input type="text" class="tulis-username" id="set-username" name="username" placeholder="Username" autocomplete="off">
-        </div></center>
-    
-        <center><div class="kotak-email"> 
-            <img src="images/Icon Email.svg" alt="" class="icon-email">
-            <input type="email" class="tulis-email" id="set-email" name="email" placeholder="Email" autocomplete="off">
-        </div></center>
-    
-        <center><div class="kotak-password">
-            <img src="images/Icon Password.svg" alt="" class="icon-password">
-            <input type="password" class="tulis-password" id="set-password" name="password" placeholder="Password" autocomplete="off">
-        </div><center>
-    
-        <div class ="terms-condition-box">
-            <input type="checkbox" name="agree" id="set-checkbox" class="checkbox" value="true">
-                <p class="term-condition-text" > I have read and agree to the<span class="term-condition-text-2"> <a href="//">privacy policy, terms and condition</a></span></p> 
-           
-    
-            <!-- <span class="term-condition-text-2"><a href="//">terms and condition</a></span> -->
-        </div>
+    <center>
+        <form action="" name="formset" method="post" class="formSubmit" onsubmit="return validasi()">
+            <div class="kotak-username"> 
+                <img src="images/Icon Us.svg" alt="" class="icon-person">
+                <input type="text" class="tulis-username" id="set-username" name="username" placeholder="Username" autocomplete="off">
+            </div>
         
-        <div class="tombol-create">
-            <!-- <h4 class="tulis-create"></h4>   -->
-            <input type="hidden" class="submit-create" value="Create an Account" name="create">
-            <div onclick="validasi()" class="submit-createe">Create an Account</div>
-        </div>
-
-        <h4 class="text-account" >Already have an account? <span class="text-account-2"> <a href="signIn.php">Sign In </a></span></h4>
-    </form>
+            <div class="kotak-email"> 
+                <img src="images/Icon Email.svg" alt="" class="icon-email">
+                <input type="email" class="tulis-email" id="set-email" name="email" placeholder="Email" autocomplete="off">
+            </div>
+        
+            <div class="kotak-password">
+                <img src="images/Icon Password.svg" alt="" class="icon-password">
+                <input type="password" class="tulis-password" id="set-password" name="password" placeholder="Password" autocomplete="off">
+            </div>
+        
+            <!-- <div class ="terms-condition-box">
+                <input type="checkbox" name="agree" id="set-checkbox" class="checkbox" value="true">
+                    <p class="term-condition-text" > I have read and agree to the<span class="term-condition-text-2"> <a href="//">privacy policy, terms and condition</a></span></p>
+            </div> -->
+            
+            <div class="tombol-create">
+                <!-- <h4 class="tulis-create"></h4>   -->
+                <input type="hidden" class="submit-create" value="Create an Account" name="create">
+                <div onclick="validasi()" class="submit-createe">Create an Account</div>
+            </div>
+    
+            <h4 class="text-account" >Already have an account? <span class="text-account-2"> <a href="signIn.php">Sign In </a></span></h4>
+        </form>
+    </center>
     
     <?php
     
     if(isset($_POST['username'])){
         $username = $_POST['username'];
         $email = $_POST['email'];
-        $password = md5($_POST['password']);
-
+        $password = $_POST['password'];
+        
         $q = mysqli_query($db, "select * from users where username='$username'");
         $cek = mysqli_num_rows($q);
-
+        
         if($cek > 0){
             echo "<script>alert('Username already used, please use another username');</script>";
         } else {
@@ -77,6 +76,7 @@
             else if($cek2 > 0){
                 echo "<script>alert('Email already used, please use another email');</script>";
             } else{
+                $password = md5($_POST['password']);
                 $datasimpan = mysqli_query($db, "insert into users set
                 username = '$username',
                 email = '$email',
